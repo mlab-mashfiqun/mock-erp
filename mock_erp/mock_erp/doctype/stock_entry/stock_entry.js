8476cb87c23af315cbac63041b0cf04897dbccb6
Mashfiqun <bddac99@BDDAC99s-MacBook-Air.local>
2025-09-02 19:06:36 +0600

    Added all the doctypes

---
+++ mock_erp/mock_erp/doctype/stock_entry/stock_entry.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Mashfiqun Nabi and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Stock Entry", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ mock_erp/mock_erp/doctype/stock_entry_item/stock_entry_item.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Mashfiqun Nabi and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class StockEntryItem(Document):
+	pass
